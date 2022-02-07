afc87e05614dd2d1129b241be8f68eb6f1ad60fd
Dmitrii Sviridov <dm.sviridov99@yandex.ru>
2022-02-07 19:06:38 +0300

    delete "value ="

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class AnimalsCage {
 
     @Autowired
-    @Qualifier(value = "dog")
+    @Qualifier("dog")
     private Animal animal;
     @Autowired
     private Timer timer;
